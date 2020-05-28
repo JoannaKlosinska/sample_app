607486e6c0cb45511786e85d00c3b3c903675607
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2020-05-28 13:37:32 +0200

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,6 @@ require 'test_helper'
         assert_select "a[href=?]", about_path
         assert_select "a[href=?]", contact_path
         get signup_path
-        assert_select "title", full_title("Signup")
+        assert_select "title", full_title("Sign up")
     end
 end
