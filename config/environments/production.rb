1973db5facb401b979381ff52ff9cdc2803687bc
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2020-07-09 13:03:59 +0200

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
@@ -100,6 +101,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
