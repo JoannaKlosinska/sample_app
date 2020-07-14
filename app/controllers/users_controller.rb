988bb55c36085177ccb4cfcac22a03d1891eb0b3
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2020-07-14 20:14:03 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,66 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-    before_action :correct_user, only: [:edit, :update]
-    before_action :admin_user, only: :destroy
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
     
-    def index
-        @users = User.where(activated: true).paginate(page: params[:page])
-    end
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
     
-    def show
-        @user = User.find(params[:id])
-        redirect_to root_url and return unless @user.activated?
-        @microposts = @user.microposts.paginate(page: params[:page])
-    end
+  def show
+  	@user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
 
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            @user.send_activation_email
-            flash[:info] = "Please chceck your email to activate your account."
-            redirect_to root_url
-        else
-          render 'new'
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please chceck your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
     end
+  end
   
-    def edit
-        @user = User.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
   
-    def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            flash[:success] = "Profile updated"
-            redirect_to @user
-        else
-            render 'edit'
-        end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+	end
 
-    def destroy
-        User.find(params[:id]).destroy
-        flash[:success] = "User deleted"
-        redirect_to users_url
-    end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.following.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
 
   private
 
@@ -58,12 +72,12 @@ class UsersController < ApplicationController
   
   # Confirms the correct user.
   def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
   
   # Confirms an admin user.
   def admin_user
-      redirect_to(root_url) unless current_user.admin?
+    redirect_to(root_url) unless current_user.admin?
   end
 end
